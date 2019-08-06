0c566ffb2872dc997573f0b3b6296da7a873fc69
dinesh kumar <dk944176@gmail.com>
2019-08-06 19:15:20 +0530

    Enter

--- folders/python/javatpoint/armstrong.py
+++ folders/python/javatpoint/armstrong.py
@@ -2,11 +2,11 @@ n= int(input("enter a number: "))
 sum=0
 temp=n
 
-while(temp>0):
+while temp>0:
     d=temp%10
     sum=sum+d**3
     temp=n//10
-if sum==n:
+if n==sum:
     print("armsrong")
 else:
     print("not armstrong")    
